8d385a70d1d1043771572668bc4033338699efcd
Jair_Junior83 <jaircezar83@gmail.com>
2025-01-02 14:43:39 -0300

    Adicionando o postinstall para rodar prisma generate

--- src/controller/control.ts
+++ src/controller/control.ts
@@ -2,6 +2,10 @@ import { Request, Response } from "express";
 import { usuarioType } from "../types/usuario";
 import { prisma } from "../libs/prisma";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 export const Cadastro = async (req: Request, res: Response): Promise<void> => {
   try {
@@ -49,6 +53,50 @@ export const Cadastro = async (req: Request, res: Response): Promise<void> => {
     res
       .status(500)
       .json({ msg: "Aconteceu um erro no servidor, tente mais tarde" });
+    return;
+  }
+};
+
+export const login = async (req: Request, res: Response) => {
+  try {
+    const { email, password }: usuarioType = req.body;
+    if (!email || !password) {
+      res.status(400).json({ msg: "Email e senha são obrigatórios" });
+      return;
+    }
+    console.log("Email:", email); // Verifique se o email está correto
+    console.log("Password:", password); // Verifique se a senha está correta
+
+    const user = await prisma.usuario.findUnique({
+      where: { email },
+    });
+    console.log("User:", user);
+    if (!user) {
+      res.json({ msg: "Credenciais inválidas" });
+      return;
+    }
+
+    const passwordVerificado = await bcrypt.compare(password, user.password);
+    if (!passwordVerificado) {
+      res.json({ mmsg: "Credenciais inválidas" });
+      return;
+    }
+
+    const token = jwt.sign({ id: user.id }, process.env.secret as string, {
+      expiresIn: "1h",
+    });
+
+    res.status(200).json({
+      msg: "Login Realizado com Sucesso",
+      status: true,
+      userId: user.id,
+      token,
+    });
+    console.log(token);
+    return;
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ msg: "Erro no servidor, favor tentar mais tarde" });
     return;
   }
 };
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,9 +2,8 @@ import { Router } from "express";
 import * as controler from "../controller/control";
 import { uploadImg } from "../midleware/upload";
 
-
 const route = Router();
 
-route.post("/cadastro", uploadImg.single("imagem"), controler.Cadastro)
-
+route.post("/cadastro", uploadImg.single("imagem"), controler.Cadastro);
+route.post("/login", controler.login);
 export default route;
